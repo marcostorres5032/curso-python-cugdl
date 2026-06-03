fbffc7eeab804bcc8d863afa9bf57db503731227
Marcos Uriel Torres Plascencia <marcos.torres5032@alumnos.udg.mx>
2026-06-02 19:31:57 -0600

    Asistente financiero.py

---
+++ Sesion 1/Ayudante Financiero.py
@@ -0,0 +1,23 @@
+# 1  -  Definicion de variables
+presupuesto = 500
+transporte = 200
+comida = 200
+material_escolar = 100
+# 2 - Calculo del gasto total y dinero restante
+gasto_total = transporte + comida + material_escolar
+dinero_restante = presupuesto - gasto_total
+# 3 - Mostrar resultados
+print("Tu presupuesto es de $", presupuesto, "pesos.")
+print("Tu gasto total es de $", gasto_total, "pesos.")
+print("Tu dinero restante es de $", dinero_restante, "pesos.")
+
+if dinero_restante > 0:
+    print("Te quedan $", dinero_restante, "pesos después de cubrir tus gastos.")
+    if dinero_restante >= 100:
+        print("¡Buen trabajo! Tienes un buen margen de ahorro!") 
+    if dinero_restante < 100:
+        print("Cuidado, estás cerca de gastar todo tu dinero. Considera reducir algunos gastos.")
+if dinero_restante < 0:
+    print("No puedes cubrir estos gastos")
+if dinero_restante == 0:
+    print("Gastaste todo tu dinero. Intenta ahorrar un poco a la próxima.")
